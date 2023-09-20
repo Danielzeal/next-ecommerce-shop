f1bfc130a14645d617ff6e67608880e1c797ebe2
Uanhumen Daniel <danieluanhumen@gmail.com>
2023-09-20 04:45:41 +0100

    orders page update

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -3,7 +3,7 @@ import Container from "@/components/Container";
 import { BASEURL } from "@/constant";
 
 const getOrders = async () => {
-  const res = await fetch(`${BASEURL}/orders`, {
+  const res = await fetch(`${BASEURL}/api/orders`, {
     method: "GET",
     cache: "no-store",
     headers: headers(),
@@ -22,20 +22,23 @@ const OrdersPage = async () => {
         {orders && orders.length > 0 ? (
           <table className='w-full table-fixed border-b-2 border-gray-200'>
             <thead>
-              <tr className='text-lg font-semibold'>
+              <tr className='text-lg font-semibold text-center'>
                 <td className='text-start w-[300px] lg:w-full'>ID</td>
-                <td className='w-[150px]'>STATUS</td>
                 <td className='w-[250px]'>DELIVIRY STATUS</td>
-                <td className='w-[150px]'>AMOUNT</td>
+                <td className='w-[250px]'>AMOUNT</td>
+                <td className='w-[q50px]'>DATE</td>
               </tr>
             </thead>
             <tbody>
               {orders.map((order) => (
-                <tr key={order.id} className='border-2 border-gray-200'>
-                  <td className='py-3 px-2'>{order.id}</td>
-                  <td>{order.status}</td>
+                <tr
+                  key={order.id}
+                  className='border-2 border-gray-200 py-4 even:bg-gray-100 odd:bg-slate-200 text-center'
+                >
+                  <td className='text-start'>{order.id}</td>
                   <td>{order.deliveryStatus}</td>
                   <td>{order.amount}</td>
+                  <td>{order.createdAt.slice(0, 10)}</td>
                 </tr>
               ))}
             </tbody>
